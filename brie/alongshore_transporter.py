Fixed indexing problem in calculation of ai_eq and gam, ah_star, c_d, basin_width.

brie/alongshore_transporter.py
@@ -220,7 +220,7 @@ def calc_coast_diffusivity(
     wave_height=1.0,
     wave_period=10.0,
     berm_ele=2.0,
-    n_bins=180,  # LVB changed from 181 to 180
+    n_bins=181,  # LVB changed from 181 to 180
 ):
     r"""Calculate sediment diffusion along a coastline. Corresponds to Equations 37-39 in NLT19 [1]_, with formulations from
     AM06 [2]_.
@@ -261,19 +261,19 @@ def calc_coast_diffusivity(
 
     # all_angles, step = np.linspace(-89.5, 89.5, n_bins, retstep=True)
     # all_angles = np.deg2rad(all_angles)
-
-    all_angles, step = np.linspace(
-        -90.0, 90.0, 180, retstep=True
-    ) # Changed to match the original code, must be checked
+    all_angles, step = np.linspace(-np.pi / 2.0, np.pi / 2.0, n_bins, retstep=True)
+    # all_angles, step = np.linspace(
+    #     -90.0, 90.0, n_bins+1, retstep=True
+    # ) # Changed to match the original code, must be checked
 
     # all_angles, step = np.linspace(-90, 90, n_bins+1, retstep=True)  # same
-    all_angles = np.deg2rad(all_angles)
+    # all_angles = np.deg2rad(all_angles)
 
     d_sf = 8.9 * wave_height
 
     # e_phi_0 = wave_pdf(all_angles) * np.deg2rad(step)
-    e_phi_0 = wave_pdf(all_angles) * np.deg2rad(step)  # same as wave_pdf in master except its not
-    e_phi_0[0] = 0 # e_phi_[0] was set to zero in origina repo
+    e_phi_0 = wave_pdf(all_angles) * step  # same as wave_pdf in master except its not
+
     # e_phi_0 = np.concatenate(([0], e_phi_0)) shifting to right
     # e_phi_0 = e_phi_0[0:-1] shifting to right
     #Roya--here we needed to add "np.deg2rad"
@@ -289,7 +289,7 @@ def calc_coast_diffusivity(
     diff_phi0_theta = (
         -(
             AlongshoreTransporter.K
-            / (1.9 + d_sf) #berm ele value was 2 but the org code used 1.9 value here, must be checked
+            / (berm_ele + d_sf) #berm ele value was 2 but the org code used 1.9 value here, must be checked
             * wave_height ** 2.4
             * wave_period ** 0.2
         )
@@ -323,6 +323,7 @@ def calc_coast_diffusivity(
             ),
         )
     ] # This was changed to match the original code.
+    coast_diff = np.interp(0 - shoreline_angles, all_angles, coast_diff_phi0_theta)
     # coast_diff = np.interp(
     #     -shoreline_angles*180/np.pi, all_angles, coast_diff_phi0_theta
     # )  # this is D above, evaluated at theta
@@ -379,7 +380,6 @@ def _build_tridiagonal_matrix(diagonal, lower=None, upper=None):
 
 
 def _build_matrix(
-    shoreline_angles,
     shoreline_x,
     wave_distribution,
     dy=1.0,
@@ -407,7 +407,7 @@ def _build_matrix(
     float or array of float
         Alongshore transport along the shoreline.
     """
-
+    shoreline_angles = calc_shoreline_angles(shoreline_x, spacing=dy)
     #shoreline_angles = calc_shoreline_angles(shoreline_x, spacing=dy)
 
     coast_diff, _ = calc_coast_diffusivity(
@@ -430,8 +430,12 @@ def _build_matrix(
     #     a_max=None,
     # )
 
-    r_ipl = coast_diff * dt / (2.0 * dy ** 2)
-
+    #r_ipl = coast_diff * dt / (2.0 * dy ** 2)
+    r_ipl = np.clip(
+        coast_diff * dt / (2.0 * dy ** 2),
+        a_min=0.0,
+        a_max=None,
+    )
 
     mat = _build_tridiagonal_matrix(1.0 + 2.0 * r_ipl, lower=-r_ipl, upper=-r_ipl)
 
@@ -461,7 +465,7 @@ class AlongshoreTransporter:
     >>> transporter.update()
     """
 
-    K = calc_alongshore_transport_k(gravity=9.81)
+    K = calc_alongshore_transport_k()
 
     def __init__(
         self,
@@ -547,8 +551,8 @@ class AlongshoreTransporter:
     #
     #     return mat.tocsc(), rhs
 
-    def update(self, x_s_dt, x_s):
-        self._dx_dt = x_s_dt
+    def update(self):
+        # self._dx_dt = x_s_dt
         # self._shoreline_x = x_s
         # self._dy
         self._shoreline_angles = calc_shoreline_angles(
@@ -591,7 +595,7 @@ class AlongshoreTransporter:
 
         # calculates diffusivity and then returns the tridiagonal matrix and right-hand-side of Equation 41 in NLT19
         mat, rhs, _ , self._coast_diff = _build_matrix(
-            self._shoreline_angles,
+            # self._shoreline_angles,
             self._shoreline_x,
             self._wave_distribution,
             dy=self._dy,

brie/inlet_spinner.py
@@ -302,7 +302,7 @@ def organize_inlet(inlet_idx, ny):
     """
 
     # inlet_idx_mat is just inlet_idx concatenated into a single array and made a float so we can use NaNs
-    print(inlet_idx)
+    # print(inlet_idx)
     inlet_idx_mat = np.hstack(inlet_idx).astype(float)
     inlet_all_idx = np.sort(inlet_idx_mat)
     inlet_all_idx_idx = np.argsort(inlet_idx_mat)
@@ -955,9 +955,9 @@ class InletSpinner:
 
         self._time = 0
 
-    def update(self, h_b, z): #updated value of h_b, and z were only updated in brie module and were not sent to this intel_spinner module.
-        self._h_b = h_b
-        self._z = z
+    def update(self): #updated value of h_b, and z were only updated in brie module and were not sent to this intel_spinner module.
+        # self._h_b = h_b
+        # self._z = z
         self._time += int(self._dt)
         self._time = int(self._time)
         self._time_index += 1 # update time index at each iteration

scripts/plot_brie_matlab.py
@@ -13,7 +13,7 @@ from scipy.io import loadmat
 from brie.brie import Brie
 
 os.chdir(
-    "/Users/Lexi/PycharmProjects/brie/tests/test_brie_matlab"
+    "/Users/rsahrae/PycharmProjects/pythonProject_CE_roya/scripts/"
 )
 
 # subset of indices for testing grid discretization
@@ -119,8 +119,13 @@ for ii in inputs_dt:
             Qinlet_total_py[ii, jj] + Qoverwash_total_py[ii, jj]
         )
 
+        inlet_age_tmp = brie._inlets._inlet_age.copy()
+        for i in range(len(inlet_age_tmp)):
+            inlet_age_tmp[i] = [inlet_age_tmp[i][0],inlet_age_tmp[i][1][0]]
+
+
         inlet_age_py = np.array(
-            brie._inlet_age
+            inlet_age_tmp
         )  # we are only going to look at the last model output
 
         # and now matlab output

tests/test_brie_matlab.py
@@ -9,7 +9,7 @@ import pathlib
 import numpy as np
 import pytest
 from scipy.io import loadmat
-
+import pickle as pkl
 from brie.brie import Brie
 
 DATA_DIR = pathlib.Path(__file__).parent / "test_brie_matlab"
@@ -92,25 +92,52 @@ def test_brie_matlab(test_case, n_steps):
     assert actual_inlet_mean == pytest.approx(expected_inlet_mean, rel=0.1)
 
 if __name__ == "__main__":
-    test_case = ALL_CASES[23]
-    n_steps = 128
-    brie = run_brie(
-        n_steps,
-        test_case["dt"],
-        test_case["dy"],
-        test_case["x_shoreline"],
-        test_case["wave_angle"],
-    )
-    actual_q_overwash_mean = brie._Qoverwash.mean()
-    actual_inlet_mean = brie._Qinlet.mean()
-    print("##")
-    print (actual_q_overwash_mean)
-    print("##")
-    expected_q_overwash_mean = test_case["q_overwash"][:n_steps].mean()
-    expected_inlet_mean = test_case["q_inlet"][:n_steps].mean()
+    # dt = [0.05, 0.1, 0.25, 0.50, 1]  # yr
+    # dy = [1000, 500, 250, 100, 50]  # , 10]   # m
+    dts = []
+    dys = []
+    for test_case in ALL_CASES:
+        dts.append(test_case["dt"])
+        dys.append(test_case["dy"])
 
-    assert len(brie._Qoverwash) == n_steps
-    assert len(brie._Qinlet) == n_steps
+    dts = np.unique(np.array(dts))
+    dys = np.unique(np.array(dys))
+    dys = -np.sort(-dys)
+    inputs_p1 = range(np.size(dts))
+    inputs_p2 = range(np.size(dys))
 
-    assert actual_q_overwash_mean == pytest.approx(expected_q_overwash_mean, rel=0.1)
-    assert actual_inlet_mean == pytest.approx(expected_inlet_mean, rel=0.1)
+    bries = np.empty((np.size(dts), np.size(dys)), dtype=object)
+    for test_case in ALL_CASES:
+        n_steps = 128
+        brie = run_brie(
+            n_steps,
+            test_case["dt"],
+            test_case["dy"],
+            test_case["x_shoreline"],
+            test_case["wave_angle"],
+        )
+        # if test_case["dt"] in dt and test_case["dy"] in dy:
+        ii = np.where(dts == test_case["dt"])
+        jj = np.where(dys == test_case["dy"])
+        bries[ii, jj] = brie
+            # bries.
+        # actual_q_overwash_mean = brie._Qoverwash.mean()
+        # actual_inlet_mean = brie._Qinlet.mean()
+        # print("##")
+        # print (actual_q_overwash_mean)
+        # print("##")
+        # expected_q_overwash_mean = test_case["q_overwash"][:n_steps].mean()
+        # expected_inlet_mean = test_case["q_inlet"][:n_steps].mean()
+
+    # np.savez(bries, "./a.npz")
+
+
+    # assert len(brie._Qoverwash) == n_steps
+    # assert len(brie._Qinlet) == n_steps
+
+    # assert actual_q_overwash_mean == pytest.approx(expected_q_overwash_mean, rel=0.1)
+    # assert actual_inlet_mean == pytest.approx(expected_inlet_mean, rel=0.1)
+
+    # np.save("./a.npz", bries)
+    file_ = open("./a.pkl", "wb")
+    pkl.dump(bries, file_)